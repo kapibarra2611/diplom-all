0ed132be59e5f5511e9a136831fcdc17bc8839a9
kapibarra2611 <126474813+kapibarra2611@users.noreply.github.com>
2023-06-16 16:00:52 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -19,17 +19,23 @@ class User(Base):
 
     __tablename__ = 'user'
     vk_id = sq.Column(sq.Integer, primary_key=True)
-    first_name = sq.Column(sq.String)
-    last_name = sq.Column(sq.String)
-    photos = relationship('Photos', backref='vk_user_id')
+    # first_name = sq.Column(sq.String)
+    # last_name = sq.Column(sq.String)
+    # photos = relationship('Photos', backref='vk_user_id')
 
 
-class Photos(Base):
-    """Таблица для хранения фото пользователей"""
+class Searcher(Base):
+    """Таблица для хранения данных искателя"""
 
-    __tablename__ = 'photos'
-    vk_id = sq.Column(sq.Integer, sq.ForeignKey('user.vk_id'))
-    url = sq.Column(sq.String, primary_key=True)
+    __tablename__ = 'searchers'
+    # first_name = sq.Column(sq.String)
+    # last_name = sq.Column(sq.String)
+    bdate = sq.Column(sq.Integer)
+    sex = sq.Column(sq.Integer)
+    city = sq.Column(sq.Integer)
+    vk_id = sq.Column(sq.Integer, primary_key=True)
+    offset = sq.Column(sq.Integer)
+    step = sq.Column(sq.Integer)  # 0 - bdate, 1 - sex, 2 - city
 
 
 def create_tables():
@@ -40,22 +46,15 @@ def create_tables():
         print(e)
 
 
-def add_user(id, first_name, last_name, photos=None):
+def add_user(id):
     """Добавление пользователя или партнера в базу данных.
     """
 
     if get_user(id):
         return 'Эта запись уже существует'
 
-    if photos is None:
-        photos = []
-    user = User(vk_id=id,
-                first_name=first_name,
-                last_name=last_name
-                )
+    user = User(vk_id=id)
 
-    for photo in photos:
-        user.photos.append(Photos(vk_id=id, url=photo))
     session.add(user)
 
     session.commit()
@@ -64,3 +63,23 @@ def add_user(id, first_name, last_name, photos=None):
 
 def get_user(id):
     return session.query(User).get(id)
+
+
+def pop_user():
+    user = session.query(User).first()
+    if user:
+        session.delete(user)
+        session.commit()
+        return user
+    else:
+        return None
+
+
+def fill_searcher(id, bdate, sex, city):
+    searcher = session.query(Searcher).get(id)
+
+    if searcher:
+        return
+    searcher = Searcher(vk_id=id, bdate=bdate, sex=sex, city=city, offset=0)
+    session.add(searcher)
+    session.commit()
--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import threading
 import database
 import database as db
+import re
 from config import USER_TOKEN, GROUP_TOKEN, GROUP_ID
-
-
 
 
 class VkUser:
     """Класс, использующий токен пользователя, для получения информации при помощи VK API."""
+
+    def __init__(self, id):
+        self.user_id = id
 
     def get_params(self, add_params: dict = None):
 
@@ -37,10 +39,15 @@ class VkUser:
         :param user_id: Идентификатор пользователя vk.
         :type user_id: int
                """
-        user_data = vk_user.method('users.get', {'user_ids': user_id, 'fields': 'sex, city, bdate'})
-        user_bdate = int(user_data[0]['bdate'][6:10])
-        user_city = user_data[0]['city']['id']
-        user_sex = user_data[0]['sex']
+        try:
+            user_data = vk_user.method('users.get', {'user_ids': user_id, 'fields': 'sex, city, bdate'})
+        except requests.exceptions.RequestException:
+            print(f"Не удалось получить данные пользователя для {user_id}")
+            VkBot().write_msg(user_id, 'Ошибка с нашей стороны. Попробуйте позже.')
+            exit()
+        user_bdate = int(user_data[0]['bdate'][6:10]) if user_data[0]['bdate'][6:10] else None
+        user_city = user_data[0]['city']['id'] if user_data[0]['city']['id'] else None
+        user_sex = user_data[0]['sex'] if user_data[0]['sex'] else None
 
         return user_bdate, user_city, user_sex
 
@@ -54,18 +61,21 @@ class VkUser:
             'https://api.vk.com/method/database.getCities',
             self.get_params({'country_id': 1, 'count': 1, 'q': city})
         )
+
         try:
             response = response.json()['response']['items']
             if not response:
-                VkBot().write_msg(user_id, 'Город не найден')
+                # VkBot().write_msg(user_id, 'Город не найден')
                 city = False
             else:
                 for city_id in response:
                     city = city_id['id']
+            return city
+
         except requests.exceptions.RequestException:
             print(f"Не удалось получить город пользователя для {user_id}: {response.json()['error']['error_msg']}")
             VkBot().write_msg(user_id, 'Ошибка с нашей стороны. Попробуйте позже.')
-            # db.update(user_id, db.UserPosition, position=1, offset=0)
+
             city = False
             return city
 
@@ -97,14 +107,14 @@ class VkUser:
 
             return False
 
-    def find_partner(self, bdate, sex, city):
+    def find_partner(self, bdate, sex, city, offset):
         """Метод для поиска потенциального партнера в вк и добавление его в базу данных.
         Подробнее в документации VK API <https://dev.vk.com/method/users.search>.
         """
         sex_find = (1 if sex == 1 else 2)
 
-        response = vk_user.method('users.search', {'count': 3,
-                                                   'offset': 0,
+        response = vk_user.method('users.search', {'count': 5,
+                                                   'offset': offset,
                                                    'city': city,
                                                    'country': 1,
                                                    'sex': sex_find,
@@ -115,22 +125,32 @@ class VkUser:
                                                    'has_photo': 1})
 
         for item in response['items']:
-            user_photos = vk_user.method('photos.get', {'owner_id': item['id'],
-                                                        'album_id': 'profile',
-                                                        'extended': 1,
-                                                        'count': 255})
+            if item['is_closed']:
+                continue
+            database.add_user(item['id'])
 
-            sorted_response = sorted(user_photos['items'],
-                                     key=lambda x: x['likes']['count'], reverse=True)
+        searcher = db.session.get(database.Searcher, self.user_id)
+        if not searcher:
+            searcher = database.Searcher(vk_id=self.user_id)
+        searcher.offset = offset
 
-            photos_message = []
-            for photo in sorted_response[:3]:
-                for element in photo['sizes']:
-                    if element['type'] == 'x':
-                        photos_message.append(element['url'])
-            add_user = database.add_user(item['id'], item['first_name'], item['last_name'], photos_message)
-            print(add_user)
-            VkBot().offer_partner(item['id'], item['first_name'], item['last_name'])
+        db.session.add(searcher)
+        db.session.commit()
+
+    def get_partner_from_db(self):
+        partner = database.pop_user()
+
+        if partner:
+            user = vk_user.method('users.get', {'user_ids': partner.vk_id})[0]
+            VkBot().offer_partner(user['id'], user['first_name'], user['last_name'])
+
+        else:
+            searcher = db.session.get(database.Searcher, self.user_id)
+            searcher.offset += 5
+            db.session.add(searcher)
+            db.session.commit()
+            self.find_partner(searcher.bdate, searcher.sex, searcher.city, searcher.offset)
+            self.get_partner_from_db()
 
 
 class VkBot:
@@ -139,7 +159,8 @@ class VkBot:
     def __init__(self):
         self.commands = [['привет', 'прив', 'ghbdtn', 'hi', 'hello', 'здравствуйте', 'хай'],
                          ['старт', 'поехали', 'начать', 'го', 'go', 'start'],
-                         ['пока', 'bye', 'до свидания', 'gjrf', 'выход']]
+                         ['пока', 'bye', 'до свидания', 'gjrf', 'выход'],
+                         ['далее']]
 
     def write_msg(self, user_id, message, attachment='', **kwargs):
         """Отправить сообщение пользователю.
@@ -158,7 +179,6 @@ class VkBot:
 
         vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7),
                                     'attachment': attachment, **kwargs})
-
 
     def chat_keyboard(self, buttons, button_colors):
         """Метод создания новой клавиатуры для бота.
@@ -186,23 +206,17 @@ class VkBot:
         :param last_name: Фамилия партнера.
         :type last_name: str
         """
-        db_user = database.get_user(partner_id)
+
         message = f'{first_name} {last_name}\n' \
                   f'Ссылка: @id{partner_id}'
 
-        top_photos = ''
-        for photo in db_user.photos:
-            top_photos += photo.url + ','
-        print(top_photos)
+        user_photos = VkUser(user_id).get_top_photos(partner_id)
 
-        if top_photos:
+        if user_photos:
             buttons = ['Далее']
             button_colors = [VkKeyboardColor.SECONDARY]
             keyboard = self.chat_keyboard(buttons, button_colors)
-            self.write_msg(user_id, message, top_photos, keyboard=keyboard.get_keyboard())
-
-        else:
-            return
+            self.write_msg(user_id, message, user_photos, keyboard=keyboard.get_keyboard())
 
     def processing_messages(self, user_id, message_text):
         """Основной метод обработки сообщений пользователя.
@@ -213,28 +227,92 @@ class VkBot:
         :type message_text: str
         """
 
+        regex = r"^\d{4}$"
+
         if message_text in self.commands[2]:
             self.write_msg(user_id, 'good_bye')
             return
 
         if message_text in self.commands[1]:
-            bdate, sex, city = VkUser().get_user_data(user_id)
+            bdate, sex, city = VkUser(user_id).get_user_data(user_id)
             sex_print = 'Мужской' if sex == 1 else 'Женский'
             self.write_msg(user_id, f'Ищем пару с заданными параметрами: Год рождения: {bdate}+-5, '
                                     f'Пол: {sex_print}, Город: {city}')
-            VkUser().find_partner(bdate, sex, city)
+            VkUser(user_id).find_partner(bdate, sex, city, offset=0)
+            VkUser(user_id).get_partner_from_db()
             return
 
         elif message_text in self.commands[0]:
             buttons = ['Старт']
             button_colors = [VkKeyboardColor.POSITIVE]
             keyboard = self.chat_keyboard(buttons, button_colors)
+            bdate, sex, city = VkUser(user_id).get_user_data(user_id)
+            db.fill_searcher(user_id, bdate, sex, city)
             self.write_msg(user_id, 'Привет, я бот, который умеет искать пару', keyboard=keyboard.get_keyboard())
 
-        #     todo: Добавить offset
+            if not bdate:
+                self.write_msg(user_id, 'Введите год рождения в формате "1982"')
+                searcher = db.session.query(database.Searcher).get(user_id)
+                searcher.step = 0
+                db.session.add(searcher)
+                db.session.commit()
+                return
+
+            if not sex:
+                self.write_msg(user_id, 'Введите пол (мужской/женский')
+                searcher = db.session.query(database.Searcher).get(user_id)
+                searcher.step = 1
+                db.session.add(searcher)
+                db.session.commit()
+                return
+
+            if not city:
+                self.write_msg(user_id, 'Введите город')
+                searcher = db.session.query(database.Searcher).get(user_id)
+                searcher.step = 2
+                db.session.add(searcher)
+                db.session.commit()
+                return
+
+        elif message_text in self.commands[3]:
+            VkUser(user_id).get_partner_from_db()
+            return
+
+        elif re.match(regex, message_text):
+            searcher = db.session.query(database.Searcher).get(user_id)
+            searcher.bdate = int(message_text)
+            db.session.add(searcher)
+            db.session.commit()
+            self.write_msg(user_id, 'Год рождения сохранен')
+            return
+
+        elif message_text.lower() in ['мужчина', 'женщина']:
+            if message_text.lower() == 'мужчина':
+                sex_id = 1
+            else:
+                sex_id = 2
+
+            searcher = db.session.query(database.Searcher).get(user_id)
+            searcher.sex = sex_id
+
+            db.session.add(searcher)
+            db.session.commit()
+            self.write_msg(user_id, 'Пол сохранен')
+            return
 
         else:
-            self.write_msg(user_id, 'Неизвестная команда')
+            city = VkUser(user_id).get_city(message_text)
+            if not city:
+                self.write_msg(user_id, 'Неизвестная команда')
+                return
+            searcher = db.session.query(database.Searcher).get(user_id)
+            searcher.city = city
+
+            db.session.add(searcher)
+            db.session.commit()
+            self.write_msg(user_id, 'Город сохранен')
+
+            return
 
 
 if __name__ == '__main__':
